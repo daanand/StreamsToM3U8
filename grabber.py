90337b172e40f055beb74e48dbefb7215fcd9ec0
Shadow <85603414+daanand@users.noreply.github.com>
2024-05-13 15:32:30 +0530

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -119,8 +119,8 @@ Grabs the live-streaming M3U8 file from yuppTv
     stream_info = requests.get(url, timeout=15, verify=False)
     response = stream_info.text
     soup = BeautifulSoup(stream_info.text, features="html.parser")
-     print(stream_info.text)
-
+    print(soup.prettify())
+    
     if '.m3u8' not in response or stream_info.status_code != 200:
         print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
         return
