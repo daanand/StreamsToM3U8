a52364996d9f80f57e4f2433b9682947950ccd2e
Shadow <85603414+daanand@users.noreply.github.com>
2024-05-13 14:31:19 +0530

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -175,7 +175,7 @@ Grabs the live-streaming M3U8 file from YouTube
     response = stream_info.text
     soup = BeautifulSoup(stream_info.text, features="html.parser")
 
-    if '&ads.user=0' not in response or stream_info.status_code != 200:
+    if 'channel_name' not in response or stream_info.status_code != 200:
         print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
         return
     end = response.find('channel_name') + 5
